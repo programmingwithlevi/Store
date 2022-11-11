fbc895ce2179e4d96884c322e6514f91aba4d788
Levi <programmingwithlevi@gmail.com>
2022-11-11 17:38:49 +0700

    first commit

---
+++ lib /client.js
@@ -0,0 +1,30 @@
+// import sanityClient from '@sanity/client';
+// import imageUrlBuilder from '@sanity/image/url';
+
+// export const client = sanityClient({
+//     projectId: 'j47x9mj3',
+//     dataset: 'production',
+//     apiVersion: '2022-19-10',
+//     useCdn: true,
+//     token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+// });
+
+// const builder = imageUrlBuilder(client);
+
+// export const urlFor = (source) => builder.image(source);
+
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+  projectId: 'j47x9mj3',
+  dataset: 'production',
+  apiVersion: '2022-10-21',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
